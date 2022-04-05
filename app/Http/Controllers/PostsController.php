334eee17dc28ae962132bc556c9a65e509803b1e
Eston Pillay <estonpillay@Estons-MacBook-Air.local>
2022-04-05 20:20:09 +0200

    Added repository, model and controller boilerplate code, validation

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,9 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StorePostRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class PostsController extends Controller
 {
-    //
+
+    public $postRepository;
+
+    public function __construct(\App\Repository\PostRepository $postRepository)
+    {
+        $this->postRepository = $postRepository;
+    }
+
+    public function get()
+    {
+        return $this->postRepository->getAll();
+    }
+
+    public function createPosts(StorePostRequest $request)
+    {
+        $validated = $request->validate();
+
+        $this->postRepository->createPost([
+            'user_id' => Auth::user()->id,
+            'title' => $validated->safe()->only('title'),
+            'text' => $validated->safe()->only('text'),
+        ]);
+    }
+
 }
--- app/Models/Posts.php
+++ app/Models/Posts.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Posts extends Model
 {
     use HasFactory;
+
+    public $table = 'posts';
+
+    public $fillable = [
+      'user_id',
+      'title',
+      'text',
+    ];
 }
